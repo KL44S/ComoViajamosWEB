44036e2a3d4cde986bcc287c1a330e07c097bd49
KL44S <agustinbinci@hotmail.com>
2018-10-19 14:50:49 -0300

    cambios

---
+++ model/review.js
@@ -0,0 +1,55 @@
+﻿function Review(datetimeFrom, datetimeUntil, travelFeelingId,
+    travelFeelingReasonIds, transportId, transportBranchId,
+    transportBranchOrientationId, captchaToken) {
+    //Prueba
+    if (datetimeFrom === undefined) {
+        this.datetimeFrom = null;
+    }
+    else {
+        this.datetimeFrom = datetimeFrom;
+    }
+
+    if (datetimeUntil === undefined) {
+        this.datetimeUntil = null;
+    }
+    else {
+        this.datetimeUntil = datetimeUntil;
+    }
+
+    if (travelFeelingId === undefined) {
+        this.travelFeelingId = null;
+    }
+    else {
+        this.travelFeelingId = travelFeelingId;
+    }
+
+    if (travelFeelingReasonIds === undefined) {
+        this.travelFeelingReasonIds = null;
+    }
+    else {
+        this.travelFeelingReasonIds = travelFeelingReasonIds;
+    }
+
+    if (transportId === undefined) {
+        this.transportId = null;
+    }
+    else {
+        this.transportId = transportId;
+    }
+
+    if (transportBranchId === undefined) {
+        this.transportBranchId = null;
+    }
+    else {
+        this.transportBranchId = transportBranchId;
+    }
+
+    if (transportBranchOrientationId === undefined) {
+        this.transportBranchOrientationId = null;
+    }
+    else {
+        this.transportBranchOrientationId = transportBranchOrientationId;
+    }
+
+    this.captchaToken = captchaToken;
+}
